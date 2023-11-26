ab2fac100f958b657505c3e0f762448998e15af3
juan <jortegah79@gmail.com>
2023-11-26 19:10:52 +0100

    google

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const Usuario=require('../models/usuario');
 const bcrypt =require('bcryptjs');
 const usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res=response) => {
     const {email,password}=req.body
@@ -42,6 +43,51 @@ const token=await generarJWT(usuarioDB.id);
 }
 
 
+const googleSignIn = async( req, res = response ) => {
+
+    try {
+        const { email, name, picture } = await googleVerify( req.body.token );
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if ( !usuarioDB ) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+            // usuario.password = '@@';
+        }
+
+        // Guardar Usuario
+        await usuario.save();
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'Token de Google no es correcto'
+        });
+    }
+}
+    
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
